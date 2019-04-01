1060d6043b8505e3fc92ac45fc2cc0d00c3f46f7
tarberd <bernardo.mferrari@gmail.com>
2019-04-01 14:48:16 -0300

    iterative algorithms class

--- iterative/gauss_seidel.m
+++ iterative/gauss_seidel.m
@@ -2,14 +2,17 @@ function x = gauss_seidel(xi, tolerance)
 
     difference = 1;
 
-    x = xi
+    x = xi;
 
-    while difference > tolerance
+    it_cout = 0;
+    while (difference > tolerance) && (it_cout < 150)
         x(1) = (3 + x(2) - x(3))/3;
         x(2) = (5 - x(1) - x(3))/3;
         x(3) = (4 - x(1) - x(2))/2;
 
         difference = max(abs(x .- xi));
+        xi = x;
+        it_cout += 1;
     end
 
 end
--- iterative/main.m
+++ iterative/main.m
@@ -1,13 +1,6 @@
-format long;
 xi = [0 0 0]
 xe = [1 1 1]
-tol = 1e-3
-x = jacobi(xi, tol)
 
-erro = max(abs(x .- xe))
-
-x_exact = jacobi(xi, tol*tol)
-
-erro_es = max(abs(x.- x_exact))
+tolerance = 1e-3
 
 x = gauss_seidel(xi, tolerance)
