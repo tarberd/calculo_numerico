c03b1af5ad71f2cb722c5e837afee0a9316887f1
tarberd <bernardo.mferrari@gmail.com>
2019-04-22 15:06:14 -0300

    Create newton find proper root

--- newton/newton.m
+++ newton/newton.m
@@ -3,6 +3,25 @@ function x = newton(f, A, B, tolerancia)
     % achar raizes
     xi = localizar_raiz_real(f, A, B)
 
-    x = 0;
+    for i = 1 : length(xi)
+
+        diff = 10;
+        max_it_count = 0;
+        while diff > tolerancia && max_it_count < 100
+            df = tan(xi(i)) + xi(i) * sec(xi(i)) * sec(xi(i));
+
+            delta_x = -f(xi(i))/df;
+
+            x(i) = xi(i) + delta_x;
+
+            xi(i) = x(i);
+
+            diff = abs(delta_x);
+
+            max_it_count += 1;
+        end
+
+    end
+
 
 end
