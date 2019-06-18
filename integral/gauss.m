6c1a6b8ba227ec47549976fe3ad20ee75843895e
tarberd <bernardo.mferrari@gmail.com>
2019-06-18 20:30:18 -0300

    Create integration by gauss

--- integral/gauss.m
+++ integral/gauss.m
@@ -1,12 +1,12 @@
 function Gm = gauss(m, a, b, f)
 
+    % corrigir para m > 2
     C = [1 1;];
-
     t = [-1/sqrt(3) 1/sqrt(3);];
 
-    x = 0.5*(b - a).*t .+ 0.5.*(b + a);
+    x = 0.5*(b - a).*t(m, 1:m) .+ 0.5.*(b + a);
 
     y = f(x);
 
-    Gm = 0.5*(b-a)* sum( C(1:m) .* y(1:m) );
+    Gm = 0.5*(b-a)* sum( C(m, 1:m) .* y(1:m) );
 end
